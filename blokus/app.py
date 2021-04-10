97cc220e81572dd05c37fd5e0afe64b6ff85adfb
Jskallio <32382026+Jskallio@users.noreply.github.com>
2021-04-10 12:46:11 +0300

    adding stuff

--- blokus/app.py
+++ blokus/app.py
@@ -17,21 +17,31 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor.execute("PRAGMA foreign_keys=ON")
     cursor.close()
 
-class state(db.Model):
+class State(db.Model):
+    __tablename__ = 'state'
     id = db.Column(db.Integer, primary_key=True)
     placed_blocks = db.Column(db.String, nullable=False)
     turn_information = db.Column(db.Integer, db.ForeignKey("player.id"))
 
+    game = db.relationship("Game", back_populates="board_state", uselist=False)
+
+
+
 class Game(db.Model):
+    __tablename__ = 'game'
     id = db.Column(db.Integer, primary_key=True)
-    board_state = db.Column(db.Integer, db.ForeignKey("state.id"), unique=True)
+    board_state_id = db.Column(db.Integer, db.ForeignKey("state.id"), unique=True)
 
     players = db.relationship("Player", back_populates="game")
+    board_state = db.relationship("State", back_populates="game")
+
 
 
 
 class Player(db.Model):
+    __tablename__ = 'player'
     id = db.Column(db.Integer, primary_key=True)
     color = db.Column(db.String, nullable=False)
     available_blocks = db.Column(db.String)
+    game_id = db.Column(db.Integer, db.ForeignKey("game.id"))
     game = db.relationship("Game", back_populates="players")
