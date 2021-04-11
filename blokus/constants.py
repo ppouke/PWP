1b65d9502e1ffba1fc0e737f09fe9a0ef44226a8
Denhonator <elmeri.uotila@gmail.com>
2021-04-11 12:53:13 +0300

    Profiles, post player to game

--- blokus/constants.py
+++ blokus/constants.py
@@ -1,3 +1,7 @@
 MASON = "application/vnd.mason+json"
 LINK_RELATIONS_URL = "/blokus/link-relations"
 ERROR_PROFILE = "/profiles/error/"
+GAME_PROFILE = "/profiles/game/"
+PLAYER_PROFILE = "/profiles/player/"
+TRANSACTION_PROFILE = "/profiles/transaction/"
+BLOCK_PROFILE = "/profiles/block/"
