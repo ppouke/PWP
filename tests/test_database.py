b4aa7c15a875c7cb0cf1f878de8ae4b187852a4f
Jskallio <32382026+Jskallio@users.noreply.github.com>
2021-04-11 13:45:52 +0300

    fixed setup.py file

--- tests/test_database.py
+++ tests/test_database.py
@@ -44,7 +44,7 @@ def _get_player(color=1):
 
 
 def _get_game():
-    return Game(placed_blocks="0"*400)
+    return Game(handle="best game", placed_blocks="0"*400)
 
 def _get_block():
     return Block(shape=("00000"
@@ -85,6 +85,9 @@ def test_created_instance(app):
         block = _get_block()
         trans = _get_transaction()
 
+        trans.game = game
+        trans.player = player
+
         game.players.append(player)
 
         db.session.add(player)
@@ -96,15 +99,19 @@ def test_created_instance(app):
         # 2a. Check all exist
         assert Player.query.count() == 1
         assert Game.query.count() == 1
+        assert Transaction.query.count() == 1
 
         db_player = Player.query.first()
         db_game = Game.query.first()
+        db_trans = Transaction.query.first()
+        db_block = Block.query.first()
 
 
         #2b. Check relationships on both sides
         assert db_player.game == db_game
         assert db_player in db_game.players
-
+        assert db_player == db_trans.player
+        assert db_game == db_trans.game
 
         #3 Update existing models
 
@@ -113,6 +120,7 @@ def test_created_instance(app):
         db.session.add(player2)
         game.players.append(player2)
         game.placed_blocks = "1234"
+        block.shape = "0000"
         db.session.commit()
 
 
@@ -120,57 +128,52 @@ def test_created_instance(app):
         assert db_player.color == 2
         assert db_player2 in db_game.players
         assert db_game.placed_blocks = "1234"
+        assert db_block.shape = "0000"
 
         #4 Remove existing models
 
-        db_handle.session.delete(state)
-        db_handle.session.delete(game)
-        db_handle.session.delete(player)
-        db_handle.session.delete(player2)
+        db.session.delete(game)
+        db.session.delete(player)
+        db.session.delete(player2)
+        db.session.delete(trans)
+        db.session.delete(block)
 
         db_handle.session.commit()
 
         assert Player.query.count() == 0
         assert Game.query.count() == 0
-        assert State.query.count() == 0
+        assert Transaction.query.count() == 0
+        assert Block.query.count() == 0
 
-def test_state_game_one_to_one(db_handle):
-
+def test_player_ondelete_game(app):
     """
-    Test that the relationship between game and state is one to one
-    i.e. we can't assign the same board state to two games
+    Tests if players get deleted when the game is deleted
     """
 
-    state = _get_state()
-    game1 = _get_game()
-    game2 = _get_game()
+    with app.app_context():
+        player = _get_player()
+        game = _get_game()
 
-    game1.board_state = state
-    game2.board_state = state
+        game.players.append(player)
 
-    db_handle.session.add(state)
-    db_handle.session.add(game1)
-    db_handle.session.add(game2)
-    with pytest.raises(IntegrityError):
-        db_handle.session.commit()
+        db.session.add(game)
+        db.session.commit()
+        db.session.delete(game)
+        db.session.commit()
+        assert player is None
 
-def test_mandatory_columns(db_handle):
+
+def test_mandatory_columns(app):
     """
     Test mandatory columns for player and state.
     """
-    #Player
-    player = _get_player()
-    player.color = None
-    db_handle.session.add(player)
-    with pytest.raises(IntegrityError):
-        db_handle.session.commit()
 
-    db_handle.session.rollback()
+    with app.app_context():
+        #Player
+        player = _get_player()
+        player.color = None
+        db.session.add(player)
+        with pytest.raises(IntegrityError):
+            db.session.commit()
 
-    #State
-
-    state = _get_state()
-    state.placed_blocks = None
-    db_handle.session.add(state)
-    with pytest.raises(IntegrityError):
-        db_handle.session.commit()
+        db.session.rollback()
